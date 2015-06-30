fa217125c5eada616cfe6364b65ae1fff2550b69
DmitriyBalykin <b_dmitriy82@mail.ru>
2015-06-30 08:06:47 +0300

    Initial commit

---
+++ AngularReview/Scripts/app/ImageController.js
@@ -0,0 +1,17 @@
+﻿    var app = angular.module("imageApplication", []);
+
+    app.controller("ImageController", [ImageController]);
+
+    function ImageController() {
+        this.images = [
+            'Content/images/1.jpg',
+            'Content/images/2.jpg',
+            'Content/images/3.jpg',
+            'Content/images/4.jpg',
+            'Content/images/5.jpg',
+            'Content/images/6.jpg',
+            'Content/images/7.jpg',
+            'Content/images/8.jpg'
+        ];
+
+    };
---
+++ AngularReview/Scripts/app/app.js
@@ -0,0 +1,3 @@
+﻿'use strict'
+
+$("#jcarousel").jcarousel();
